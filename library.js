c5b0280ac6efb685dc2b221dfb267ebce8598d3c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-09-22 14:55:04 +0100

    able to take user input and make a book using the values passed

--- library.js
+++ library.js
@@ -1,3 +1,32 @@
+
+//DOM Elements
+const inputAuthor = document.getElementById("author");
+const inputTitle = document.getElementById("title");
+const inputPages = document.getElementById("pages");
+const inputRead = document.getElementById("toggleButton");
+const submitted = document.getElementById("submit");
+let bookAuthor
+let bookTitle
+let bookPages
+let bookRead
+
+console.log(inputAuthor.value);
+console.log(inputTitle);
+console.log(inputPages);
+console.log(inputRead.checked);
+console.log(submitted)
+
+
+submitted.addEventListener("click",function(){
+    let bookAuthor = inputAuthor.value;
+    let bookTitle = inputTitle.value;
+    let bookPages = inputPages.value;
+    let bookRead = inputRead.checked;
+    let newBook = new Book(bookAuthor,bookTitle,bookPages,bookRead);
+    console.log(newBook);
+});
+
+//Constructor
 function Book(title,author,pages,read){
     this.title = title,
     this.author = author,
