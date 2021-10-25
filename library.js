f8e351f49086a81686b98ac46a20e50cad9db53d
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-10-25 17:41:02 +0100

    refactored the code to use class

--- library.js
+++ library.js
@@ -59,12 +59,14 @@ else if(event.target.classList == "toggleButton-Book"){
 
 
 /* Constructor */
+class Book{
+    constructor(title,author,pages,read){
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = read
 
-function Book(title,author,pages,read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+    }
 }
 
 /* Functions */
